52aee176520616c5c04e2b22526d4fea7d142034
Jessica Saviano <jessica@savs.com>
2023-08-28 16:23:18 -0400

    new harness

--- harness.c
+++ harness.c
@@ -2,58 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <list.h>
+#include "list.c"
 
 
 
- //constructor
-void list_init(list_t *l, int (*compare)(const void *key, const void *with),void (*datum_delete)(void *datum)){
-    list_t *mylist;
-    mylist = (list_t*)malloc(sizeof(mylist));
-    l->head = NULL;
-    l-> compare = compare;
-    l->datum_delete = datum_delete;
-    l -> tail = NULL;
+int main(int argc, char ** argv){
+    File *file = fopen(argv[1], "r");
+    if(strcmp("echo", argv[2])== 0){
+        printf("hi");
 
-}
 
-void print_string(void *v){
-    printf('%s\n',(char *)v); 
-
-}
-void list_visit_items(list_t *l, void (*visitor)(void *v)){
-    list_t *current = l->head;
-    list_item_t *next;
-    while(current != NULL){
-        print_string(current->datum);
-        current = current-> next;
     }
-}
 
-void list_insert_tail(list_t *l, void *v) {
-    // insert input when second parameter is tail
-    // print contents of list 
-    list_item_t *item;
-    item = (list_item_t*)malloc(sizeof(list_item_t));
-    item->next = NULL;
-    l->tail = item;
-    v = malloc();
-    strncpy(v, str);
+    if(strcmp("tail", argv[2])== 0){
+        //do stuff
 
-}
 
-void list_remove_head(list_t *l){
-    //do stuff
-    //use free()
+    }
 
-//each line in each node
-//when list reaches lenght of zero, now print empty
-//remove three nodes at a time unitl empty for head
-//if theres only 2 left, remove those two, and then print empty
-//if the input file is compeltely empty just print empty and exit
+    if(strcmp("tail-remove", argv[2])== 0){
+        //do stuff
+
+
+    }
 
 
 }
-
-
 
