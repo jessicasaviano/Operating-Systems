50e54d9e0124081c373da8ff54ca42a8140b587c
nikitajeyasingh <npj5kr@virginia.edu>
2023-08-28 18:03:09 -0400

    no idea

--- harness.c
+++ harness.c
@@ -45,6 +45,21 @@ int main(int argc, char ** argv){
 
     if(strcmp("tail-remove", argv[2])== 0){
         //do stuff
+        FILE *file = fopen(argv[1] , "r");
+        list_t * newlist; 
+        list_initalize(&newlist, NULL, NULL);
+        for (int i = 3; i < argc; i++) {
+            list_insert_tail(&newlist, argv[i]);
+        }
+        while (mylist.head != NULL) {
+            list_remove_h(&newlist);
+            if (mylist.head != NULL) {
+                printf("------\n");
+            }
+        }
+        printf("Empty List\n");
+
+
 
 
     }
