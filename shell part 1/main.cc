e4c68a5ec8eebcf55f8c18bbe5ca4ff113cc049d
Jessica Saviano <jessica@savs.com>
2023-09-05 12:44:37 -0400

    shell part 1 is done!!!!

--- shell part 1/main.cc
+++ shell part 1/main.cc
@@ -12,98 +12,14 @@
 
 using namespace std;
 
-
-bool invalid_command(vector<string> &command_line){
-
-            int num = command_line.size()-1;
-            //if((command_line[0] == "<" ||command_line[0] == "<")){
-                //cout << "hey"<<endl;
-            //    return true;
-            //}          
-            
-            for(int i = 0; i < num; i++){
-              
-                //cout << "now:" << command_line[i]<< endl;
-                //cout << "and:" << command_line[i+1]<< endl;
-                if(command_line[i] == "<"){
-                   
-                    //cout << "this:"<< num-1 << endl;
-                    //cout << "that"<< i <f< endl;
-                    
-                    if (i+1 > 0 && i+1 <= num){
-                            if((command_line[i+1] == ">") || (command_line[i+1] == "<")) {
-                                return false;
-                            }
-                    }
-                    else if(num - 1 == i ){
-                        if(command_line[num - 1] != "<" || command_line[num - 1] != ">") {
-                        return true;
-                        }
-                    }
-
-                    else if (i+2 > 0 && i+2 <= num){
-                            if(command_line[i+2] == ">"){
-                                return true;
-                            }
-                    }
-                }
-                if(command_line[i] == ">"){
-                   
-                    //cout << "this:"<< num-1 << endl;
-                    //cout << "that"<< i <f< endl;
-                    if (i+1 > 0 && i+1 <= num){
-                            if((command_line[i+1] == ">") || (command_line[i+1] == "<")) {
-                                return false;
-                            }
-                    }
-                    if(num - 1 == i ){
-                        return true;
-                    }
-                    else if (i+2 > 0 && i+2 <= num){
-                            if(command_line[i+2] == "<"){
-                                return true;
-                            }
-                    
-
-                    }
-                }
-
-            }
-
-        
-            
-         
-        if (std::count(command_line.begin(), command_line.end(), ">") || std::count(command_line.begin(), command_line.end(), ">")){
-            return false;
-                }
-        else{
-        
-            return true;
-        }
-
-        
-        return false;
-
-}
-
-
-
-
  void child_and_IO(vector<string> &commands, string input, string output){
-        int status;
-    // Convert vector of strings to a vector of char*
+    int status;
     vector<char*> execute;
-    
     for (vector<string>::iterator t=commands.begin(); t!=commands.end(); ++t) {
         execute.push_back(strdup(t->c_str()));
-    }
-    
+    }  
     execute.push_back(nullptr);
-/*
-     for (vector<char*>::iterator t=execute.begin(); t!=execute.end(); ++t) {
-        cout << "Here:" << *t<<endl;
-    }
-    */
+
     int pid = fork();
     if (pid == 0) {
         // Child process
@@ -133,19 +49,12 @@ bool invalid_command(vector<string> &command_line){
             close(out_re);
         }
         }
-        
+
          execv(execute[0], execute.data());
-        perror(execute[0]);
+         perror(execute[0]);
     } else if (pid > 0) {
-        // Parent process
-        // Wait for the child process to complete
         wait(&status);
-       if (WIFEXITED(status)) {
-            // exited normally
-            
-            cout <<  commands[0] << " exit status: " << WEXITSTATUS(status) << endl;
-        } 
-       
+            cout << commands[0] << " exit status: " << WEXITSTATUS(status) << endl;
     } 
     else {
             cerr << "ERR: fork failed" << endl;
@@ -156,31 +65,23 @@ bool invalid_command(vector<string> &command_line){
             free(arg);
         }
     }
-
     execute.clear();
     execute.shrink_to_fit();
-    //exit(1);
 }
 
-    
 void parse_and_run_command(const std::string &command) {
-    
+
     std::istringstream ss(command);
     std::string token;
     vector<string> input_files;
     vector<string> output_files;
     string current_input_file = "none";
     string current_output_file = "none";
-    bool redirect_input = false;
-    bool redirect_output = false;
-
     std::vector<std::string> tokens;
     vector<string> commands = vector<string>();
     while(ss >> token){
         tokens.push_back(token);
     }
-    
-    //start parsing thru
     if(tokens.size() == 0){
         return;
     }
@@ -191,56 +92,56 @@ void parse_and_run_command(const std::string &command) {
     }
     else{
         
-         if(invalid_command(tokens) != false){
-            int number = tokens.size();
-            for(int i = 0; i < number; i++){
-                if(tokens[i] == "|"){
-                    exit(0);
+        int number = tokens.size();
+        for(int i = 0; i < number; i++){
+            if(tokens[i] == "|"){
+                exit(0);
                 }
 
             if (tokens[i] == "<") {
-            
-                redirect_input = true;
-                //redirecti_count +=1;
+                if(i == number-1){
+                    cerr << "invalid command" << endl;
+                    cout <<"invalid command:"<< command <<":" << " exit status: 255" << endl;
+                }
+                else{
+                    if(tokens[i+1] == "<" ||tokens[i+1] == ">" ){
+                        cerr << "invalid command" << endl;
+                    cout <<"invalid command:"<< command <<":" << " exit status: 255" << endl;
+
+                    }
+                 current_input_file = tokens[i+1];
+                 i+=1;
+                }
+
             } else if (tokens[i] == ">") {
-                // Output redirection detected
-                redirect_output = true;
-                //redirecto_count +=1;
-            } else if (redirect_input) {
-                current_input_file = tokens[i];
-                //cout << tokens[i];
-                redirect_input = false;
-            } else if (redirect_output) {
-                // Store the output file
-                current_output_file = tokens[i];
-                redirect_output = false;
-            } else {
-            
-                // Regular command or argument
+
+                if(i == number-1){
+                    cerr << "invalid command" << endl;
+                    cout <<"invalid command:"<< command <<":" << " exit status: 255" << endl;
+                }
+
+                else{
+                    if(tokens[i+1] == "<" ||tokens[i+1] == ">" ){
+                        cerr << "invalid command" << endl;
+                    cout <<"invalid command:"<< command <<":" << " exit status: 255" << endl;
+                    }
+   
+                 current_output_file = tokens[i+1];
+                 i+=1;
+                }
+            } else{
                     commands.push_back(tokens[i]);
+            }
                 }
             }
             
-            //for (vector<string>::iterator t=commands.begin(); t!=commands.end(); ++t) {
-              //      cout<<"this:"<<*t<<endl;
-                //    }
-            child_and_IO(commands, current_input_file, current_output_file);
-  
-         }
-         else{
-            cerr << "invalid command" << endl;
-        cout << command <<":" << " exit status: 255" << endl;
+            if(commands.size() == 0){
+                cerr << "invalid command" << endl;
+                    cout <<"invalid command:"<< command <<":" << " exit status: 255" << endl;
 
-         }
-         //test!
-         //cout << current_input_file<< endl;
-         //cout << current_output_file << endl;
-    }
-        //for (vector<string>::iterator t=commands.begin(); t!=commands.end(); ++t) {
-        //cout<<*t<<endl;
-    //}
-        
-        
+            }
+            child_and_IO(commands, current_input_file, current_output_file);
+
     tokens.clear();
     tokens.shrink_to_fit();
     }
@@ -253,34 +154,4 @@ void parse_and_run_command(const std::string &command) {
     }
     return 0;
 }
-
-    /*example shell, helped me do it all!
-    while((s = Getline())! = EOF) {
-        if((pid=fork) == 0){      //child
-            //I/O redirection
-            close(stdout);
-            fd = open(file);
-            r = execlp("...");
-            if(r<0)
-                //handle error
-        }
-        else if(pid >0){    //parent
-            //if no & in command line
-            wait(&status);
-
-    }else
-        exit(0);
-
-    }
-    
-    if (command == "exit") {
-        exit(0);
-    }
-    std::cerr << "Not implemented.\n";
-}
-
-*/
-
-
-
 
