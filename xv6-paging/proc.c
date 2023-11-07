43214fd454e37c5fccfdde0cde57e8058411b6db
Jessica Saviano <jessica@savs.com>
2023-11-07 11:33:43 -0500

    added all test files

--- xv6-paging/proc.c
+++ xv6-paging/proc.c
@@ -174,8 +174,10 @@ growproc(int n)
 
   sz = curproc->sz;
   if(n > 0){
-    if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
-      return -1;
+    //if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
+      //return -1;
+      sz += n //COMMENTED OUT THIS BC IT CAN STILL SET SZ BUT SHLDNT CREATE PAGE TABLE ENTRIES
+
   } else if(n < 0){
     if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
       return -1;
